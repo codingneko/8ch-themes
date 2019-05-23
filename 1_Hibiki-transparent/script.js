63b154e05dd5da1d28536007560fee31067a9df2
CodingNeko <codingneko@gmail.com>
2019-05-23 05:36:39 +0200

    first commit

---
+++ 1_Hibiki-transparent/script.js
@@ -0,0 +1,8 @@
+$("#post-form-inner input[type='submit']").remove();
+$("#post-form-inner form[name='post']").append('<input accesskey="s" style="margin-left:2px;" type="submit" name="post" value="New Thread">');
+
+document.querySelectorAll('.op').forEach((element) => {
+	if(!element.classList.contains('has-file')){
+		element.parentElement.querySelector('.files').style = "margin-top:0px;";
+	}
+});
