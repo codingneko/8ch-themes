89065e10c50d2dd501df5b2a9e3c2c9e3264d3a5
CodingNeko <codingneko@gmail.com>
2019-05-24 21:50:29 +0200

    fixed catalog

--- 1_Hibiki-transparent/script.js
+++ 1_Hibiki-transparent/script.js
@@ -1,5 +1,5 @@
 $("#post-form-inner input[type='submit']").remove();
-$("#post-form-inner form[name='post']").append('<input accesskey="s" style="margin-left:2px;" type="submit" name="post" value="New Thread">');
+$("#post-form-inner form[name='post']").append('<input accesskey="s" style="margin-left:2px;" type="submit" name="post" value="New post">');
 
 document.querySelectorAll('.op').forEach((element) => {
 	if(!element.classList.contains('has-file')){
